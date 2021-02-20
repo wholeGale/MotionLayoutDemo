324ff3086294f801d407b20b27ff37d17c9ca55d
xiangzhenbiao <xiangzhenbiao@hellogeek.com>
2021-02-20 20:26:59 +0800

    运动布局基本运动

---
+++ app/src/main/java/com/xzb/motionlayoutdemo/DemoActivity.kt
@@ -0,0 +1,16 @@
+package com.xzb.motionlayoutdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+
+
+    }
+
+
+}
--- app/src/main/java/com/xzb/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/xzb/motionlayoutdemo/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.xzb.motionlayoutdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_look_motion_layout).setOnClickListener(View.OnClickListener {
+            //HomeMainActivity的launchMode="singleTask"，跳转回去会先清空上面所有的activity，然后利用HomeMainActivity自身finish
+            startActivity(Intent(this@MainActivity, DemoActivity::class.java))
+        })
     }
+
 }
